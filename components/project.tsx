4ca702a80c8206712bf745252be793a7a87aabeb
SebKrusz <145293986+SebKrusz@users.noreply.github.com>
2024-02-20 21:46:19 -0800

    projects mostly functional, adding animations still, removing client tag from project

--- components/project.tsx
+++ components/project.tsx
@@ -1,25 +1,7 @@
-"use client";
-
-import React, { useRef } from "react";
-import SectionHeading from "./section-heading";
 import { projectsData } from "@/lib/data";
-import Image from "next/image";
 import { useScroll } from "framer-motion";
-
-export default function Projects() {
-	return (
-		<section>
-			<SectionHeading> My projects </SectionHeading>
-			<div>
-				{projectsData.map((project, index) => (
-					<React.Fragment key={index}>
-						<Project {...project} />
-					</React.Fragment>
-				))}
-			</div>
-		</section>
-	);
-}
+import React, { useRef } from "react";
+import Image from "next/image";
 
 type ProjectProps = (typeof projectsData)[number];
 
