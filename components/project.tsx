f1754aef491b2d4f31883bc1edda9368683beba5
SebKrusz <145293986+SebKrusz@users.noreply.github.com>
2024-02-21 22:53:52 -0800

    winner winner chicken dinner

--- components/project.tsx
+++ components/project.tsx
@@ -27,8 +27,8 @@ export default function Project({
 			{" "}
 			<section
 				className=" bg-gray-100 max-w-[42rem] border border-black/5 rounded-lg overflow-hidden sm:pr-8 relative sm:h-[20rem] even:pl-8
-    hover:bg-gray-200 transition group-even:pl-8 dark:text-white dark:bg-white/10 dark:hover:bg-white/20 ">
-				<div className="pt-4 pb-7 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] flex flex-col h-full group-even:ml-[18rem] ">
+    hover:bg-gray-200 transition sm:group-even:pl-8 dark:text-white dark:bg-white/10 dark:hover:bg-white/20 ">
+				<div className="pt-4 pb-7 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] flex flex-col h-full sm:group-even:ml-[18rem] ">
 					<h3 className="text-2xl font-semibold">{title}</h3>
 					<p className="mt-2 leading-relaxed text-gray-700 dark:text-white/70">
 						{description}
@@ -47,7 +47,7 @@ export default function Project({
 					src={imageUrl}
 					alt="Project Sebastian Worked On"
 					quality={95}
-					className="absolute top-8 -right-40 w-[28.25rem] rounded-t-lg shadow-2xl transition group-hover:scale-[1.04] group-hover:-translate-x-3 group-hover:translate-y-3 group-hover:-rotate-2 group-even:group-hover:translate-x-3 group-even:group-hover:translate-y-3 group-even:group-hover:rotate-2 group-even:right-[initial] group-even:-left-40"
+					className="absolute hidden sm:block top-8 -right-40 w-[28.25rem] rounded-t-lg shadow-2xl transition group-hover:scale-[1.04] group-hover:-translate-x-3 group-hover:translate-y-3 group-hover:-rotate-2 group-even:group-hover:translate-x-3 group-even:group-hover:translate-y-3 group-even:group-hover:rotate-2 group-even:right-[initial] group-even:-left-40"
 				/>
 			</section>
 		</motion.div>
--- lib/data.ts
+++ lib/data.ts
@@ -35,28 +35,43 @@ export const links = [
 
 export const experiencesData = [
 	{
-		title: "Graduated bootcamp",
-		location: "Miami, FL",
-		description:
-			"I graduated after 6 months of studying. I immediately found a job as a front-end developer.",
+		title: "Apprenticeship",
+		location: "British Columbia Institute of Technology, Vancouver",
+		description: "I began my apprenticeship in Electrical.",
 		icon: React.createElement(LuGraduationCap),
 		date: "2019",
 	},
 	{
-		title: "Front-End Developer",
-		location: "Orlando, FL",
+		title: "Apprentice Electrician",
+		location: "Vancouver, BC",
 		description:
-			"I worked as a front-end developer for 2 years in 1 job and 1 year in another job. I also upskilled to the full stack.",
+			"I worked as an Electrician for almost 5 years with Houle Electric, specializing in Lighting Controls and serving as a Construction Electrician across institutional, commercial, and industrial settings.",
 		icon: React.createElement(CgWorkAlt),
-		date: "2019 - 2021",
+		date: "2019 - 2023",
+	},
+	{
+		title: "Journeyman Electrician",
+		location: "Vancouver, BC",
+		description:
+			"After accumulating 8,000 hours of work experience, I passed the Red Seal Electrician exam, becoming a certified Electrician in Canada. I was awarded the title of Journeyman Electrician.",
+		icon: React.createElement(CgWorkAlt),
+		date: "2023",
+	},
+	{
+		title: "BrainStation Full-Stack Web Development Bootcamp",
+		location: "Vancouver, BC",
+		description:
+			"I completed BrainStation's Web Development Bootcamp, an immersive program designed to equip participants with the latest skills in web design and development. This intensive training honed my abilities in front-end and back-end technologies, enabling me to build responsive, user-centered websites and applications. The curriculum's hands-on approach, focused on real-world projects, has prepared me to tackle diverse web development challenges effectively.",
+		icon: React.createElement(FaReact),
+		date: "2023",
 	},
 	{
 		title: "Full-Stack Developer",
-		location: "Houston, TX",
+		location: "Vancouver, BC",
 		description:
-			"I'm now a full-stack developer working as a freelancer. My stack includes React, Next.js, TypeScript, Tailwind, Prisma and MongoDB. I'm open to full-time opportunities.",
+			"I'm now a full-stack developer working as a freelancer. My stack includes React, Node, Express, Next.js, TypeScript, Tailwind, Prisma and MySQL. I'm currently looking for full-time opportunities.",
 		icon: React.createElement(FaReact),
-		date: "2021 - present",
+		date: "Present",
 	},
 ] as const;
 
@@ -92,11 +107,7 @@ export const projectsData = [
 		imageUrl: wordanalyticsImg,
 	},
 ] as const;
-// title: "CSKB",
-// description:
-// 	"This platform is designed to empower gamers by providing a comprehensive set of tools for safe and informed Counter-Strike trading. Whether you're checking links, analyzing in-game items, or seeking influencers and websites, our knowledge base has you covered.",
-// tags: ["React", "Node", "Express", "MySQL", "SCSS/SASS", "Postman"],
-// imageUrl: rmtdevImg,
+
 export const skillsData = [
 	"HTML",
 	"CSS",
